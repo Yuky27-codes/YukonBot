31a5fbc709a068a01e006fe979cd2b402425f7e0
Yuky-27 <devyuky7@gmail.com>
2026-01-13 08:20:11 -0300

    atualizar

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ffmpeg = require('fluent-ffmpeg');
 const { Groq } = require("groq-sdk");
-const groq = new Groq({ apiKey: "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" });
+const groq = new Groq({ apiKey: "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" });
 const mongoURI = 'mongodb+srv://admin:teteu2025@cluster0.4wymucf.mongodb.net/?appName=Cluster0'; 
 
 mongoose.connect(mongoURI)
